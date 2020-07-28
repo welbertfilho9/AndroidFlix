dd30b8b03399bdd96a291a4fce665eb287e26bf0
Welbert Filho <welbertfilho9@gmail.com>
2020-07-28 15:37:18 -0300

    final 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/home';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+const Pagina404 = () => (<div>404 Página Não Encontrada</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
---
+++ src/pages/cadastro/video/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PageDefault from '../../../components/PageDefault';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
+
+function CadastroVideo() {
+    return (
+      <PageDefault>
+        <h1>
+           Cadastrar Vídeo
+        </h1>
+
+        <Link to="/cadastro/categoria">
+            Cadastrar Categoria
+        </Link>
+      </PageDefault>
+    )
+  }
+
+export default CadastroVideo;  
